31c0989a82a5855f8f3a78ca9e27ca91aface4cd
waseem2329 <waseem_2329@live.com>
2025-01-11 18:45:37 +0000

    updated

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+  'id' :1,
+  'title' : 'Desktop Supprt',
+  'location' : 'Karachi',
+  'salary' : 'Rs. 70,000',
+  },
+  {
+    'id' :2,
+    'title' : 'Desktop Supprt',
+    'location' : 'Karachi',
+  },
 
+  {
+  'id' :3,
+  'title' : 'Desktop Supprt',
+  'location' : 'Karachi',
+  'salary' : 'Rs. 70,000',
+  },
+
+  {
+  'id' :4,
+  'title' : 'Desktop Supprt',
+  'location' : 'Karachi',
+  'salary' : 'Rs. 70,000',
+  },
+]
+  
 @app.route('/')
-def hello_world():
-  return "Hello World"
+def hello_waseem():
+  return render_template('home.html', jobs=JOBS)
+  
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
